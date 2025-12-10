be18bca3d890dcdbea1b8061a2188a451b462f43
Quentin Gliech <quentingliech@gmail.com>
2025-12-10 16:42:04 +0100

    Use a specific error type instead of Box<dyn Error>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::sync::LazyLock;
 use opentelemetry::InstrumentationScope;
 use opentelemetry::metrics::Meter;
 
+#[allow(dead_code, reason = "Might be unused on unsupported platforms")]
 static METER: LazyLock<Meter> = LazyLock::new(|| {
     let scope = InstrumentationScope::builder(env!("CARGO_PKG_NAME"))
         .with_version(env!("CARGO_PKG_VERSION"))
@@ -18,14 +19,32 @@ static METER: LazyLock<Meter> = LazyLock::new(|| {
 #[cfg(target_os = "linux")]
 mod linux;
 
+/// The error returned when process metrics initialisation fails.
+#[derive(Debug)]
+pub struct InitError {
+    source: Box<dyn std::error::Error + Send + Sync + 'static>,
+}
+
+impl std::fmt::Display for InitError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Could not initialise process metrics")
+    }
+}
+
+impl std::error::Error for InitError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(self.source.as_ref())
+    }
+}
+
 /// Initialise process metrics collection.
 ///
 /// # Errors
 ///
 /// Returns an error if initialisation fails.
-pub fn init() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+pub fn init() -> Result<(), InitError> {
     #[cfg(target_os = "linux")]
-    self::linux::init()?;
+    self::linux::init().map_err(|source| InitError { source })?;
 
     Ok(())
 }
